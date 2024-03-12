97691f2dd6d4941c66cb2902b22cde781a113edf
andrewsalvadr <andrewsalva88@gmail.com>
2024-03-12 17:18:14 +0800

    mobile navigation

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+    name: "AndrewBlog",
+    url: "https://example.com",
+    description: "Blog and Portfolio using nextjs 14, velite, tailwind, typescript and shadcn",
+    author: "Andrew Salvador",
+    links: {
+        twitter: "",
+        github: "",
+        personalSite: "",
+    }
+};
+
+export type SiteConfig = typeof siteConfig;
