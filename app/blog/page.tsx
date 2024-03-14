11bb53ac6ff0aaf06aa5b43864f4e99284a6b3fa
andrewsalvadr <andrewsalva88@gmail.com>
2024-03-14 23:17:03 +0800

    change description of blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -51,7 +51,7 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
         <div className="flex-1 space-y-4">
           <h1 className="inline-block font-black text-4xl lg:text-5xl">Blog</h1>
           <p className="text-xl text-muted-foreground">
-              Navigating Coding, AI and Career Shifters: Your Gateway to a Tech Career
+              Explore Coding, Solution and AI.
           </p>
           <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
           <CategoryFilter/>
