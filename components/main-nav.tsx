71cbf0ba5e02ddb22719ee0436ca5199dab6ce45
andrewsalvadr <andrewsalva88@gmail.com>
2024-04-19 09:41:47 +0800

    Add logo, add blog post about firebase intro

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,15 +1,24 @@
+// main-nav.tsx
 "use client";
 
 import { siteConfig } from "@/config/site";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
+import { useTheme } from "next-themes";
+import Image from 'next/image';
 
 export function MainNav() {
   const pathname = usePathname();
+  const { resolvedTheme } = useTheme(); // Use resolvedTheme here
+
+  const logoUrl = resolvedTheme === 'dark' ? '/logo-dark.png' : '/logo-light.png';
+
+
   return (
     <nav className="flex items-center space-x-4 lg:space-x-6">
       <Link href="/" className="mr-6 flex items-center space-x-2">
+        <Image src={logoUrl} alt="Logo" width={40} height={30} />
         <span className="font-bold">{siteConfig.name}</span>
       </Link>
       <Link
